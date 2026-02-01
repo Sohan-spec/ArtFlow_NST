c122e2a8d6ab9dc74b3b9f1a8d12469bdeaea84d
sohan <sohanreddy06@gmail.com>
2026-02-01 23:00:12 +0530

    video generation failed, updated it

--- utils/video_utils.py
+++ utils/video_utils.py
@@ -1,23 +1,33 @@
 import os
 import subprocess
+import shutil
+import tempfile
 
-def create_video_from_results(results_path,img_format):
-    import shutil #this is to manipulate like folders and files
-    output_file_name='merged_output.mp4'
-    fps=24
-    first_frame=0
-    number_of_frames_to_process=len(os.listdir(results_path))
+def create_video_from_results(results_path, img_format):
+    output_file_name = 'cool_output.mp4'
+    fps = 2 #you aint flash buddy, sooo vid gon be slow, face it
+
+    ffmpeg = shutil.which("ffmpeg") #this like allows you to manipulate files and folers
+    if ffmpeg is None:
+        print("ffmpeg wasnt found in path")
+        return
+
+    video_save_path = os.path.join(results_path, output_file_name)
+
+    frames = sorted(
+        f for f in os.listdir(results_path)
+        if f.endswith(img_format[1]))
     
-    ffmpeg='ffmpeg'
-    if shutil.which(ffmpeg):
-        img_name_format='%' + str(img_format[0])+'d'+(img_format[1])
-        pattern=os.path.join(results_path,img_name_format)
-        video_save_path=os.path.join(results_path,output_file_name)
-        
-        trim_video_comm=['-start_number',str(first_frame),'-vframes',str(number_of_frames_to_process)] #this is to start from the first frame and the go until the last
-        input_options=['-r',str(fps),'-i',pattern] #read all the images in the pattern path at 24 fps(so if 300 images-> 300/24, you get 12.5s vid)
-        encoding_options=['-c:v', 'libx264', '-crf', '18', '-pix_fmt', 'yuv420p']
-        #libx264 is a video codedc format, crf is the quality level(lower is better) and yuv420p is pixel format supported by most devices
-        subprocess.call([ffmpeg,*input_options,*trim_video_comm,*encoding_options,video_save_path])
-    else:
-        print("ffmpeg was not installed, install it and try again later")
+    with tempfile.NamedTemporaryFile(mode="w", suffix=".txt", delete=False) as f:
+        concat_file = f.name
+        for frame in frames:
+            f.write(f"file '{os.path.join(results_path, frame)}'\n")
+
+    comm = [ffmpeg,"-y","-r", str(fps),"-f", "concat","-safe", "0","-i", concat_file,"-c:v", "libx264","-crf", "18","-pix_fmt", "yuv420p",video_save_path]
+     #libx264 is a video codedc format, crf is the quality level(lower is better) and yuv420p is pixel format supported by most devices
+    result = subprocess.run(comm, capture_output=True, text=True)
+
+    os.remove(concat_file)
+
+    if result.returncode != 0:
+        print("ffmpeg failed")
