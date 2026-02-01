d982ca0c97c475cc43c391b2113b7e6cf666a3eb
sohan <sohanreddy06@gmail.com>
2026-02-01 23:02:42 +0530

    added a couple args, updated vid gen wala, changed noise value from 90 to 30, iterations from 1000 to 600

--- neural_style_transfer.py
+++ neural_style_transfer.py
@@ -1,7 +1,7 @@
 import utils.utils as utils
 from utils.video_utils import create_video_from_results
 import torch
-from torch.optim import LBFGS
+from torch.optim import LBFGS,Adam
 from torch.autograd import Variable
 import numpy as np
 import os
@@ -22,7 +22,7 @@ def build_loss(neural_net,optimizing_img,target_representations,content_feature_
             gram = utils.gram_matrix(x)
             current_style_representation.append(gram)
     for  (gram_target, gram_current) in zip(target_style_representation, current_style_representation):
-        style_loss +=torch.nn.MSELoss(reduction='sum')(gram_target, gram_current)# as sum penalizes more than mean
+        style_loss +=torch.nn.MSELoss(reduction='sum')(gram_target[0], gram_current[0]) #sum is less penalizign than mean, and this for me proved to produce better results
     style_loss/=len(target_style_representation)
     tv_loss=utils.total_variation_loss(optimizing_img)
     total_loss=config['content_weight']*content_loss + config['style_weight']*style_loss+ config['tv_weight']*tv_loss
@@ -56,7 +56,7 @@ def neural_style_transfer(config):
     style_image=utils.prepare_image(style_image_path,config['height'],device)
     
     if config['initial_method']=='random':
-        random_noise_image=np.random.normal(loc=0,scale=90.0,size=content_image.shape).astype(np.float32)
+        random_noise_image=np.random.normal(loc=0,scale=30.0,size=content_image.shape).astype(np.float32)
         initial_img=torch.from_numpy(random_noise_image).float().to(device)
     elif config['initial_method']=='content':
         initial_img=content_image
@@ -75,8 +75,7 @@ def neural_style_transfer(config):
     target_style_representation=[utils.gram_matrix(x) for cnt,x in enumerate(style_img_set_of_feature_maps) if cnt in style_feature_maps_indices_names[0]]
     target_representations=[target_content_representation,target_style_representation]
     
-    num_of_iterations={"lbfgs":1000} 
-    
+    num_of_iterations={"lbfgs":600} 
     if config['optimizer']=='lbfgs':
         optimizer=LBFGS((optimizing_img,),max_iter=num_of_iterations['lbfgs'],line_search_fn='strong_wolfe')
         cnt=0
@@ -89,7 +88,7 @@ def neural_style_transfer(config):
             if total_loss.requires_grad:
                 total_loss.backward()
             with torch.no_grad():
-                print(f"using LBFGS, iteration : {cnt}, total_loss : {total_loss.item():.4f}, content_loss : {config['content_weight'] * content_loss.item():.4f}, total_variaton loss : {config['tv_weight'] * tv_loss.item():.4f}")
+                print(f"using LBFGS, iteration : {cnt:03}, total_loss : {total_loss.item():12.4f}, content_loss : {config['content_weight'] * content_loss.item():12.4f}, total_variaton loss : {config['tv_weight'] * tv_loss.item():12.4f}")
                 utils.save_and_maybe_display(optimizing_img,dump_path,config,cnt,num_of_iterations[config['optimizer']])
                 if config['should_display']==True:
                     if (cnt%50)==0:
@@ -98,11 +97,10 @@ def neural_style_transfer(config):
             return total_loss
         optimizer.step(closure)
     if config['saving_freq'] > 0:
-        print("Generating video from saved frames...")
+        print("yo vid comin up buddy, enjoy the slow mo")
         create_video_from_results(dump_path, config['img_format'])
     else:
-        print("Skipping video generation (saving_freq = -1)")
-
+        print("skipped vid gen as freq=-1")
     return dump_path
 
 
@@ -114,16 +112,16 @@ if __name__ == "__main__":
     img_format = (4, '.jpg')
     
     parser=argparse.ArgumentParser()
-    parser.add_argument("--content_img_name",type=str,help="choose any image from the data/content-images or feel free to add your own images there",default='taj_mahal.jpg')
+    parser.add_argument("--content_img_name",type=str,help="choose any image from the data/content-images or feel free to add your own images there",default='lion.jpg')
     parser.add_argument("--style_img_name",type=str,help="choose any image from the data/style-images or feel free to add your own images there",default='vg_starry_night.jpg')
     parser.add_argument("--height",type=int,help="The height you want to resize your image to (ex: like 224,256) etc",default=400)
-    parser.add_argument("--content_weight",type=float,help="Weights for content loss",default=1e5)
-    parser.add_argument("--style_weight",type=float,help="Weights for style loss",default=3e4)
-    parser.add_argument("--tv_weight",type=float,help="Weights for total variation loss",default=1e0)
+    parser.add_argument("--content_weight",type=float,help="weights for content loss",default=1e5)
+    parser.add_argument("--style_weight",type=float,help="weights for style loss",default=3e4)
+    parser.add_argument("--tv_weight",type=float,help="weights for total variation loss",default=1e0)
     parser.add_argument("--optimizer",type=str,help="i only choose LBFGS as its the best, no choice sorry :)",default='lbfgs')
-    parser.add_argument("--model",type=str,help="Choose your model",choices=['vgg16','vgg19'],default='vgg19')
-    parser.add_argument("--initial_method",type=str,help="Choose your initial method, random noise image, the content image or the style image",choices=['random','content','style'],default='content')
-    parser.add_argument("--saving_freq",type=int,help="how often your images get saved (cycles), choose -1 for only saving the last output image",default=-1)
+    parser.add_argument("--model",type=str,help="choose your model",choices=['vgg16','vgg19'],default='vgg19')
+    parser.add_argument("--initial_method",type=str,help="choose your initial method, random noise image, the content image or the style image",choices=['random','content','style'],default='content')
+    parser.add_argument("--saving_freq",type=int,help="how often your images get saved (cycles), choose -1 for only saving the last output image",default=100)
     parser.add_argument("--should_display",type=bool,help="do you want the output of features to be displayed",default=False)
     args=parser.parse_args()
     
